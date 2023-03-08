4088ebfa080570338c87ad80ff69089754a02114
utinono-kanji <tyuggfdshh@gmail.com>
2023-03-08 16:01:15 +0900

    か

--- PHP.job/PHP1-8Theme.php
+++ PHP.job/PHP1-8Theme.php
@@ -1,18 +1,29 @@
 <?php
+
+/*3の倍数のとき、 Fizz! と出力します。
+5の倍数のとき、 Buzz! と出力します。
+3の倍数かつ5の倍数のとき、 FizzBuzz!! と出力します。(15の倍数、と考えるのはナシです。)
+3の倍数でも、5の倍数でもない場合は、その数を出力します。
+それを1〜100まで実施してください。*/ 
 $num=1;
-while($num <101){
+while($num <=100){
   echo '<br>';
   echo $num;
 
-  if($num % 3 && $num % 5){
+  if($num % 3==0 && $num % 5==0){
     echo "FizzBuzz!!";
-  }else if($num % 3){
-    echo "Fizz!";
-  }else if($num % 5){
-    echo "Buzz!!";
+  }else if($num % 3==0){
+    echo "Fizz!" ;
     
+  }else if($num % 5==0){
+    echo "Buzz!";
+  }else{
+    echo "";
   }
   $num++;
+
 }
+
+
 
 ?>
--- PHP.job/carenji2.php
+++ PHP.job/carenji2.php
@@ -1,14 +1,14 @@
 <?php
 //0~100まで表示
 $num =0;
-while($num < 101){
+while($num <= 100){
   echo $num;
   $num++;
   echo '<br>';
 
 }
   
-for($i=0;$i<101;$i++){
+for($i=0;$i<=100;$i++){
   echo $i;
   echo '<br>';
 }
