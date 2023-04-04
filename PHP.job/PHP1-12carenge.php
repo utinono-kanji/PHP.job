52f1c403c5148fa38c376534117ed34a7cf82713
utinono-kanji <tyuggfdshh@gmail.com>
2023-04-04 16:08:06 +0900

    課題

--- PHP.job/PHP1-12carenge.php
+++ PHP.job/PHP1-12carenge.php
@@ -13,7 +13,7 @@
 
   //商品の税込価格を計算しましょう
   //①税率を定数TAXで作成しましょう。消費税は10%とします。
-define('TAX',0.10);
+define('TAX',1.1);
 
 
 
@@ -22,13 +22,14 @@ define('TAX',0.10);
   $products = ["鉛筆" =>100 , "消しゴム" =>150 , "物差し" => 500];
 
   //③税率を計算する関数を用意します。
- function getTriangleArea($subtotal){
- return $subtotal * TAX;
+ function getTriangleArea($value){
+return $value * TAX;
  }
   //④繰り返し文を使って画面に指定の文字を表示しましょう！
-foreach($products as $key => $value){
+foreach($products as $key => $value ){
+
   echo '<br>';
-  echo  $key. "の税込み価格は" .$value . "です";
+  echo  $key. "の税込み価格は" . getTriangleArea($value) . "です";
 }
 
 /*下記の表示にする
